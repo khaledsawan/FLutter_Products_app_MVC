12054908c022cb138b2a7e48356f257ac770a962
Hornet7 <khaled963sawan@gmail.com>
2022-07-09 18:36:27 +0300

    controller

--- lib/controller/products_controller.dart
+++ lib/controller/products_controller.dart
@@ -4,25 +4,18 @@ import 'package:get/get.dart';
 import '../service/model/product_model.dart';
 import '../service/repository/popular_product_repo.dart';
 
-
 class ProductController extends GetxController {
   late PopularProductRepo popularProductRepo;
 
   ProductController({required this.popularProductRepo});
 
   late List<dynamic> _productList = [];
-  List<dynamic> get popularProductList => _productList;
-
-
+  List<dynamic> get ProductList => _productList;
+  late List<dynamic> _myproductList = [];
+  List<dynamic> get MyProductList => _myproductList;
 
   bool _isLoaded = false;
   bool get isLoaded => _isLoaded;
-
-  int _quantity = 0;
-  int get quantity => _quantity;
-
-  int _inCartItems = 0;
-  int get inCartItems => _inCartItems + _quantity;
 
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.GetProductList();
@@ -37,11 +30,55 @@ class ProductController extends GetxController {
     }
   }
 
+  Future<void> getmyproduct() async {
+    Response response = await popularProductRepo.GetProductList();
+    if (response.statusCode == 200) {
+      _productList = [];
+      _productList.addAll(Product.fromJson(response.body).products);
+      print(_productList);
+      _isLoaded = true;
+      update();
+    } else {
+      print("No products");
+    }
+  }
 
+  Future<void> addproduct() async {
+    Response response = await popularProductRepo.GetProductList();
+    if (response.statusCode == 200) {
+      _productList = [];
+      _productList.addAll(Product.fromJson(response.body).products);
+      print(_productList);
+      _isLoaded = true;
+      update();
+    } else {
+      print("No products");
+    }
+  }
 
+  Future<void> updateproduct() async {
+    Response response = await popularProductRepo.GetProductList();
+    if (response.statusCode == 200) {
+      _productList = [];
+      _productList.addAll(Product.fromJson(response.body).products);
+      print(_productList);
+      _isLoaded = true;
+      update();
+    } else {
+      print("No products");
+    }
+  }
 
-
-
-
-
+  Future<void> deletteproduct() async {
+    Response response = await popularProductRepo.GetProductList();
+    if (response.statusCode == 200) {
+      _productList = [];
+      _productList.addAll(Product.fromJson(response.body).products);
+      print(_productList);
+      _isLoaded = true;
+      update();
+    } else {
+      print("No products");
+    }
+  }
 }
